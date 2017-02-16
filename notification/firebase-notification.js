2b41067ff60545ca6b27a412944e8414ee62ceca
raulbolsantos <raulbolsantos@gmail.com>
2017-02-16 00:05:46 -0200

    parâmetro destino da notificação

--- notification/firebase-notification.js
+++ notification/firebase-notification.js
@@ -1,5 +1,3 @@
-// Android_key: f_fp2i48hGU:APA91bFRg_yHtt5MCEGOahl0dwjyBVGWfyk1kmtQjzgRN5Bvh296BfqXQW_hoyJiRD-IWncCF-st93PHNtI7CqmdccGqgxig1GpAvnaOZy5fORDaw5LWxIJvlAdT1X1v8VWm-of0dBhC
-
 var request = require('request');
 
 // Set the headers
@@ -11,33 +9,38 @@ var headers = {
 
 // Função pública
 
-exports.sendNotification = function(notificationObj){
-	// Configure the request
-	var options = {
-		url: "https://fcm.googleapis.com/fcm/send",
-		headers : headers,
-		json : true,
-		body : 
-		{
-			 "to" : "dcoPL3dKkK4:APA91bFjsXHeDDF9nCwARFBmBopVy-Mr9BQlgHTDH1R0O1A3U-JS2FfAV3AqPrQV8mdr_4sOjam1XOU7OM7IsIPOMvy27DzwhgJqGFhhJsUPbgFgXLNq_PL8Udq9VEmU1u23j43pDLvI",
-			 "notification" : {
-			 "title" : "Mensagem para o Firebase",
-			 "body" : "Teste firebase"
-			 }, 
-			 "data" : {
-			 "nome" : "Raul",
-			 "sobrenome" : "Bolsonaro"
-			 }
-		}
-		
-	}
+exports.sendNotification = function(notification){
+	if(notification !== undefined){
+		var destination = notification.destination
+		if(destination !== undefined){
+			// Configure the request
+			var options = {
+				url: "https://fcm.googleapis.com/fcm/send",
+				headers : headers,
+				json : true,
+				body : 
+				{
+					 "to" : destination,
+					 "notification" : {
+						 "title" : "Mensagem para o Firebase",
+						 "body" : "Teste firebase"
+					 }, 
+					 "data" : {
+					 	"nome" : "Raul",
+					 	"sobrenome" : "Bolsonaro"
+					 }
+				}
+				
+			}
 
-	// Dispara a requisição
-	request.post(options, function (error, response, body) {
-		console.log(options);
-		console.log("Erro: "+error);
-		console.log("Resposta: "+response);
-		console.log("Show me your body: : "+body);
-	});
+			// Dispara a requisição
+			request.post(options, function (error, response, body) {
+				console.log(options);
+				console.log("Erro: "+error);
+				console.log("Resposta: "+response);
+				console.log("Show me your body: : "+body);
+			});
+		}
+	}
     
 };
