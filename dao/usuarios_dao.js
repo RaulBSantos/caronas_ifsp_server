728f63473f4cf94fc779f256e5bffc40ef4ef513
raulbolsantos <raulbolsantos@gmail.com>
2017-05-15 01:16:26 -0300

    Devolvia um array, foi preciso obj[0]

--- dao/usuarios_dao.js
+++ dao/usuarios_dao.js
@@ -1,267 +1,167 @@
-/*
-	Esquema do banco:
-	
-	1 Raul pede carona p/ Maria
-	2 Maria aceita dar carona para Raul
-	3 Raul oferece carona para Pedro
-	4 Pedro aceita carona de Raul
+			// Objetos para manusear promises
+			var assert = require('assert');
+			var Promise = require('mpromise');
+			var promise = new Promise;
+			var async = require('async');
 
-	listener
+			var mongoose = require('mongoose');
+			mongoose.set('debug', true);
+			mongoose.connect('mongodb://localhost/caronas');
+
+			var db = mongoose.connection;
+
+			var User = undefined; // Teste
+			var Ride = undefined; // Teste
 
 
-	1 )
-{
-		nome : "Raul",
-		record : 1238976,
-		location : { latitude : 8743.97, longitude : 17613.89 },
-		pendingRides :  [ { { user : { nome : "Maria", record : 545234 , location : 
-						{ latitude : 9999.97, longitude : 10000.89 } }
-						  },  driver : false, dateRequest : "20/04/2017 10:20:00", dateResponse : null  } ], 
-	  confirmedRides : []
-},
-	
-	
-{
-		nome : "Maria",
-		record : 545234,
-		location : { latitude : 9999.97, longitude : 10000.89 },
-		pendingRides :  [ { { user : { nome : "Raul", record : 1238976 , location : 
-						{ latitude : 8743.97, longitude : 17613.89 } }
-						  },  driver : true, dateRequest : "20/04/2017 10:20:00", dateResponse : null } ],
-		confirmedRides : []
-						  
-}
+			db.on('error', console.error.bind(console, 'connection error:'));
+			db.once('open', function() {
+			  // Conectado 
+			  // Criando um Schema de carona
+			  var rideSchema = mongoose.Schema({
+				 user : { name : {type : String }, record : { type : String }, phone : {type : String}, email : {type : String},
+					location : { latitude : {type : Number}, longitude : {type : Number}}
+				 },
+				 driver : { type : Boolean },
+				 dateRequest : { type : Date},
+				 dateResponse : { type : Date}
+			  });
 
-	2 )
+			  Ride = mongoose.model('Ride', rideSchema);
 
-{
-		nome : "Raul",
-		record : 1238976,
-		location : { latitude : 8743.97, longitude : 17613.89 },
-		confirmedRides :  [ { { user : { nome : "Maria", record : 545234 , location : 
-						{ latitude : 9999.97, longitude : 10000.89 } }
-						  },  driver : false, dateRequest : "20/04/2017 10:20:00", dateResponse : "21/04/2017 22:59:40"  } ], 
-	  pendingRides : []
-}
-	
-	
-{
-		nome : "Maria",
-		record : 545234,
-		location : { latitude : 9999.97, longitude : 10000.89 },
-		pendingRides : []
-		
-		confirmedRides :  [ { { user : { nome : "Raul", record : 1238976 , location : 
-						{ latitude : 8743.97, longitude : 17613.89 } }
-						  },  driver : true, dateRequest : "20/04/2017 10:20:00", dateResponse : "21/04/2017 22:59:40" } ],
-		
-						  
-},
+			  exports.Ride = Ride;
 
- 	3 )
+			  // Criando um Schema do usuário
+			  var userSchema = mongoose.Schema({
+			  	name : {type : String},
+			  	record : {type : String, unique : true},
+			  	firebaseId : {type : String, unique: true},
+			  	canGiveRide : {type : Boolean},
+			  	phone : {type : String},
+			  	email : {type : String},
+			  	location : { latitude : {type : Number}, longitude : {type : Number}},
+			  	ridesOffer : [rideSchema],
+			  	pendingRides : [{ name : {type : String},
+			  			record : {type : String},
+					  	phone : {type : String},
+					  	email : {type : String},
+					  	location : { latitude : {type : Number}, longitude : {type : Number}},
+			  		}],
+			  	ridesAsked : [{
+			  		rideId 		: {type : String}, 
+			  		driverId 	: {type : String} }]
+			  });
+			  // Convertendo o Schema em um Model do usuário
+			  User = mongoose.model('User', userSchema);
 
-{
-		nome : "Raul",
-		record : 1238976,
-		location : { latitude : 8743.97, longitude : 17613.89 },
-		confirmedRides :  [ { { user : { nome : "Maria", record : 545234 , location : 
-						{ latitude : 9999.97, longitude : 10000.89 } }
-						  },  driver : false, dateRequest : "20/04/2017 10:20:00", dateResponse : "21/04/2017 22:59:40"  } ], 
-	  pendingRides : [ { user : { nome : "Pedro", record : 090807 , location : 
-						{ latitude : 323232.97, longitude : 2332.22 } }
-						  },  driver : true, dateRequest : "23/04/2017 10:20:00", dateResponse : null } ] ]
-},
+			  // Teste
+			  addPendingRequestRide('136208-9', '136263-1');
 
-{
-  nome : "Pedro", 
-  record : 090807 , 
-  location : { latitude : 323232.97, longitude : 2332.22 }, 
-  confirmedRides : [],
-  pendingRides : [ { user : { nome : "Raul", record : 1238976 , location : 
-						{ latitude : 8743.97, longitude : 17613.89 } }
-						  },  driver : false, dateRequest : "23/04/2017 10:20:00", dateResponse : null } ] ]
-  
-},
+			  // Tornando pública
+			  exports.User = User;
+			});
 
-	4 )
+			// Funções públicas
 
-{
-		nome : "Raul",
-		record : 1238976,
-		location : { latitude : 8743.97, longitude : 17613.89 },
-		confirmedRides :  [ 
-  		        { { user : { nome : "Maria", record : 545234 , location : 
-  						  { latitude : 9999.97, longitude : 10000.89 } }
-  						    },  driver : false, dateRequest : "20/04/2017 10:20:00", dateResponse : "21/04/2017 22:59:40"  },
-  						    
-  			      { user : { nome : "Pedro", record : 090807 , location : 
-  						  { latitude : 323232.97, longitude : 2332.22 } }
-  						    },  driver : true, dateRequest : "23/04/2017 10:20:00", dateResponse : "25/04/2017 02:55:13" } ]
-  					], 
-	  pendingRides : []
-},
+			// Salva um usuário
+			exports.saveUser = function(user){
+				console.log('saveUser()'+user);
+				user.save();
+				/*
+				var promise = user.save();
 
-{
-  nome : "Pedro", 
-  record : 090807 , 
-  location : { latitude : 323232.97, longitude : 2332.22 }, 
-  confirmedRides : [{ user : { nome : "Raul", record : 1238976 , location : 
-						{ latitude : 8743.97, longitude : 17613.89 } }
-						  },  driver : false, dateRequest : "23/04/2017 10:20:00", dateResponse : "25/04/2017 02:55:13" }],
-  pendingRides : []
-  
-}
+				assert.ok(promise instanceof require('mpromise'));
+				*/
+			    
+			};
 
-*/
+			exports.findUserByRecord = function(record, callback){
+				exports.User.findOne({record : record}, callback);
+			};
+			// Update or insert 
+			exports.findOneAndUpdate = function(user){
+				// Deleta o novo id criado, para o documento continuar com o mesmo id
+				// caso já exista
+				var userObj = user.toObject();
+				delete userObj._id;
+				exports.User.findOneAndUpdate(
+					{record : user.record}, // Query para buscar
+					userObj,// Documento que será inserido caso nada seja encontrado
+					{upsert: true, new : true}, function(err, doc){
+						if(err){
+							console.error(err);
+						}else{
+							console.log(doc);
+						}
 
-// Objetos para manusear promises
-var assert = require('assert');
-var Promise = require('mpromise');
-var promise = new Promise;
-var async = require('async');
+					});
+			};
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/caronas');
+			addPendingRequestRide = function(askingUserRecordId, offerUserRecordId){
+				// Find other User
+				console.log('Starting addPendingRequestRide');
+				var askingUser = undefined;
+				var offerUser = undefined;
 
-var db = mongoose.connection;
+				async.parallel([
+			        //Load user origin
+			        function(callback) {
+			        	User.find({ 'record' : offerUserRecordId }
+			        		, 'name record phone email location', 
+			        		function (err, doc_user) {
+			        			if (err) return handleError(err);
+			        			offerUser = doc_user[0];
+			        			console.log('Finded  %s %s ', doc_user.name, doc_user.record);
+			        			callback();
+							});
+			        },
+			        //Load user destination
+			        function(callback) {
+			            User.find({ 'record' : askingUserRecordId }
+			        		, 'name record phone email location', 
+			        		function (err, doc_user) {
+			        			if (err) return handleError(err);
+			        			delete doc_user._id;
+			        			askingUser = doc_user[0];
+			        			console.log('Nome: '+ doc_user[0].name + ' Total: '+JSON.stringify(doc_user[0]));
+			        			callback();
+							});
+			        }
+			    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+			    	// Update
+			    	User.update(
+					    { 'record' : offerUserRecordId },
+					    { "$push": { "pendingRides":  { user : askingUser, driver : false, dateRequest : Date.now() }  } },
+				    function(err,numAffected) {
+				       // something with the result in here
+				       console.log('Updated ' + offerUserRecordId + '. Rows affected: ' + JSON.stringify(numAffected));
+				    });
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // Conectado 
-  // Criando um Schema de carona
-  var rideSchema = mongoose.Schema({
-	 user : { name : {type : String }, record : { type : String },
-		location : { latitude : {type : Number}, longitude : {type : Number}}
-	 },
-	 driver : { type : Boolean },
-	 dateRequest : { type : Date},
-	 dateResponse : { type : Date}
-  });
+				    User.update(
+					    { 'record' : askingUserRecordId },
+					    { "$push": { "pendingRides":  { user : offerUser, driver : true, dateRequest : Date.now() } } },
+				    function(err,numAffected) {
+				       // something with the result in here
+				       console.log('Updated ' + askingUserRecordId + '. Rows affected: ' + JSON.stringify(numAffected));
+				    });
+			    });
+			};
 
-  var Ride = mongoose.model('Ride', rideSchema);
+			exports.findAllUsers = function(callback){
+				exports.User.find(function (err,users){
+					if(err) return console.error(err);
+					console.log(users);
+					// Executa função de callback com o retorno
+					callback(users);
+				})
+			};
 
-  exports.Ride = Ride;
-
-  // Criando um Schema do usuário
-  var userSchema = mongoose.Schema({
-  	name : {type : String},
-  	record : {type : String, unique : true},
-  	firebaseId : {type : String, unique: true},
-  	canGiveRide : {type : Boolean},
-  	phone : {type : String},
-  	email : {type : String},
-  	location : { latitude : {type : Number}, longitude : {type : Number}},
-  	ridesOffer : [Object],
-  	ridesAsked : [{
-  		rideId 		: {type : String}, 
-  		driverId 	: {type : String} }]
-  });
-  // Convertendo o Schema em um Model do usuário
-  var User = mongoose.model('User', userSchema);
-  // Tornando pública
-  exports.User = User;
-});
-
-// Funções públicas
-
-// Salva um usuário
-exports.saveUser = function(user){
-	console.log('saveUser()'+user);
-	user.save();
-	/*
-	var promise = user.save();
-
-	assert.ok(promise instanceof require('mpromise'));
-	*/
-    
-};
-
-exports.findUserByRecord = function(record, callback){
-	exports.User.findOne({record : record}, callback);
-};
-// Update or insert 
-exports.findOneAndUpdate = function(user){
-	// Deleta o novo id criado, para o documento continuar com o mesmo id
-	// caso já exista
-	var userObj = user.toObject();
-	delete userObj._id;
-	exports.User.findOneAndUpdate(
-		{record : user.record}, // Query para buscar
-		userObj,// Documento que será inserido caso nada seja encontrado
-		{upsert: true, new : true}, function(err, doc){
-			if(err){
-				console.error(err);
-			}else{
-				console.log(doc);
+			exports.saveRideIntoUser = function(ride, user){
+				console.log(user);
+				user.ridesOffer.push(ride);
+				console.log("saving ride in user...");
+				exports.findOneAndUpdate(user);
 			}
 
-		});
-};
 
-exports.addPendingRequestRide = function(askingUserRecordId, offerUserRecordId){
-	// Find other User
-	console.log('Starting addPendingRequestRide');
-	var askingUser = undefined;
-	var offerUser = undefined;
 
-	async.parallel([
-        //Load user origin
-        function(callback) {
-        	User.find({ 'record' : offerUserRecordId }
-        		, 'name record location', 
-        		function (err, doc_user) {
-        			if (err) return handleError(err);
-        			offerUser = doc_user;
-        			console.log('Finded ' + offerUserRecordId);
-        			callback();
-				});
-        },
-        //Load user destination
-        function(callback) {
-            User.find({ 'record' : askingUserRecordId }
-        		, 'name record location', 
-        		function (err, doc_user) {
-        			if (err) return handleError(err);
-        			askingUser = doc_user;
-        			console.log('Finded ' + askingUserRecordId);
-        			callback();
-				});
-        }
-    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
-    	// Update
-    	User.update(
-		    { 'record' : offerUserRecordId },
-		    { "$push": { "pendingRides": { 'user' : askingUser, 'driver' : false, 'requestDate' : new Date(), 'responseDate' : undefined } } },
-	    function(err,numAffected) {
-	       // something with the result in here
-	       console.log('Updated ' + offerUserRecordId + '. Rows affected: ' + numAffected);
-	    });
-
-	    User.update(
-		    { 'record' : askingUserRecordId },
-		    { "$push": { "pendingRides": { 'user' : offerUser, 'driver' : true, 'requestDate' : new Date(), 'responseDate' : undefined } } },
-	    function(err,numAffected) {
-	       // something with the result in here
-	       console.log('Updated ' + askingUserRecordId + '. Rows affected: ' + numAffected);
-	    });
-    });
-};
-
-exports.findAllUsers = function(callback){
-	exports.User.find(function (err,users){
-		if(err) return console.error(err);
-		console.log(users);
-		// Executa função de callback com o retorno
-		callback(users);
-	})
-};
-
-exports.saveRideIntoUser = function(ride, user){
-	console.log(user);
-	user.ridesOffer.push(ride);
-	console.log("saving ride in user...");
-	exports.findOneAndUpdate(user);
-}
-
-// Teste
-addPendingRequestRide('136208-9', '136263-1');
